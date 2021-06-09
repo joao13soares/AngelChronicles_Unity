9aa4b877b7f343100a88da957bfa9ebc28a9bbd7
joao13soares <joaopbsoares13@hotmail.com>
2021-06-09 22:36:26 +0100

    SpikyEnemy (texture and sounds)

---
+++ Assets/Scripts/Enemies/SpikeEnemy/SpikesSound.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesSound : MonoBehaviour
+{
+    [SerializeField] AudioClip spikesSFX;
+    void PlaySpikesSound() => this.GetComponent<AudioSource>().PlayOneShot(spikesSFX);
+}
--- Assets/SpikyAnim.cs
+++ Assets/SpikyAnim.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class SpikyAnim : MonoBehaviour
 {
-    [SerializeField]
-    Animator bodyAnimator;
-    [SerializeField]
-    Animator spikesAnimator;
+    [SerializeField] Animator spikesAnimator;
     
     enum SpikyState
     {
@@ -15,21 +12,17 @@ public class SpikyAnim : MonoBehaviour
         Opening
     }
 
-
     // Update is called once per frame
     void Update()
     {
         bool isPlayerDetected = this.GetComponent<FieldOfViewDetection>().isPlayerDetected;
-        Debug.Log(isPlayerDetected);
 
         if(isPlayerDetected)
         {
-            bodyAnimator.SetInteger("animState", (int)SpikyState.Opening);
             spikesAnimator.SetInteger("animState", (int)SpikyState.Opening);
         }
         else
         {
-            bodyAnimator.SetInteger("animState", (int)SpikyState.Retract);
             spikesAnimator.SetInteger("animState", (int)SpikyState.Retract);
         }
     }
